440a161913ac54b76092043bda58d023b6cd4561
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-06-13 23:04:15 -0300

    test: add test for the process method

--- ml_scripts_eda/credit_score_2/test_processing.py
+++ ml_scripts_eda/credit_score_2/test_processing.py
@@ -2,6 +2,141 @@ from numpy import NaN
 import pandas as pd
 from processing import Cs2DataSetPreProcessing
 
+# 0x163a5
+# 0x23922
+# 0xfd6e
+
+
+def test_process():
+    mock_df = pd.DataFrame(
+        {
+            "ID": {0: "0xfd6e", 1: "0x163a5", 2: "0x23922"},
+            "Customer_ID": {
+                0: "CUS_0x3dd8",
+                1: "CUS_0x2bdc",
+                2: "CUS_0x24bc",
+            },
+            "Month": {0: "January", 1: "August", 2: "January"},
+            "Name": {
+                0: "Anirban Nagy",
+                1: "Greg Roumeliotisp",
+                2: "Chuck Mikolajczakb",
+            },
+            "Age": {0: "24", 1: "29", 2: "48"},
+            "SSN": {0: "851-44-4923", 1: "454-30-4171", 2: "664-52-6558"},
+            "Occupation": {0: "Entrepreneur", 1: "Writer", 2: "Engineer"},
+            "Annual_Income": {
+                0: "32928.14",
+                1: "119223.09",
+                2: "116224.68",
+            },
+            "Monthly_Inhand_Salary": {
+                0: 2948.0116666666668,
+                1: 10046.2575,
+                2: 9873.39,
+            },
+            "Num_Bank_Accounts": {0: 0, 1: 0, 2: 0},
+            "Num_Credit_Card": {0: 2, 1: 6, 2: 3},
+            "Interest_Rate": {0: 5, 1: 2, 2: 8},
+            "Num_of_Loan": {0: "0", 1: "0", 2: "1"},
+            "Type_of_Loan": {
+                0: "Home Loan",
+                1: "Car Loan",
+                2: "Personal Loan",
+            },
+            "Delay_from_due_date": {0: 10, 1: 25, 2: 10},
+            "Num_of_Delayed_Payment": {0: "2", 1: "4", 2: "3"},
+            "Changed_Credit_Limit": {0: "8.63", 1: "4.53", 2: "5.01"},
+            "Num_Credit_Inquiries": {0: 0.0, 1: 4.0, 2: 0.0},
+            "Credit_Mix": {0: "Bad", 1: "Bad", 2: "Good"},
+            "Outstanding_Debt": {0: "929.04", 1: "14.96", 2: "1388.02"},
+            "Credit_Utilization_Ratio": {
+                0: 28.99555955433552,
+                1: 37.305406548467005,
+                2: 28.45691504450972,
+            },
+            "Credit_History_Age": {
+                0: "29 Years and 7 Months",
+                1: "22 Years and 3 Months",
+                2: "32 Years and 1 Months",
+            },
+            "Payment_of_Min_Amount": {0: "No", 1: "No", 2: "No"},
+            "Total_EMI_per_month": {0: 0.0, 1: 0.0, 2: 59.58605365589474},
+            "Amount_invested_monthly": {
+                0: "311.2433285195494",
+                1: "201.1301814828621",
+                2: "626.101931891704",
+            },
+            "Payment_Behaviour": {
+                0: "Low_spent_Small_value_payments",
+                1: "High_spent_Large_value_payments",
+                2: "Low_spent_Medium_value_payments",
+            },
+            "Monthly_Balance": {
+                0: "273.5578381471173",
+                1: "1043.495568517138",
+                2: "581.6510144524012",
+            },
+            "Credit_Score": {0: "Good", 1: "Poor", 2: "Standard"},
+        }
+    )
+
+    result = Cs2DataSetPreProcessing.process(mock_df)
+
+    expected_result = pd.DataFrame(
+        {
+            "Age": {0: 24, 1: 29, 2: 48},
+            "Occupation": {0: "Entrepreneur", 1: "Writer", 2: "Engineer"},
+            "Monthly_Inhand_Salary": {0: 2948.01, 1: 10046.26, 2: 9873.39},
+            "Num_Bank_Accounts": {0: 0, 1: 0, 2: 0},
+            "Num_Credit_Card": {0: 2, 1: 6, 2: 3},
+            "Interest_Rate": {0: 5, 1: 2, 2: 8},
+            "Num_of_Loan": {0: 0, 1: 0, 2: 1},
+            "Delay_from_due_date": {0: 10, 1: 25, 2: 10},
+            "Num_of_Delayed_Payment": {0: 2, 1: 4, 2: 3},
+            "Changed_Credit_Limit": {0: 8.63, 1: 4.53, 2: 5.01},
+            "Num_Credit_Inquiries": {0: 0, 1: 4, 2: 0},
+            "Credit_Mix": {0: "Bad", 1: "Bad", 2: "Good"},
+            "Outstanding_Debt": {0: 929.04, 1: 14.96, 2: 1388.02},
+            "Credit_Utilization_Ratio": {
+                0: 28.99555955433552,
+                1: 37.305406548467005,
+                2: 28.45691504450972,
+            },
+            "Credit_History_Age": {0: 355, 1: 267, 2: 385},
+            "Payment_of_Min_Amount": {0: "No", 1: "No", 2: "No"},
+            "Total_EMI_per_month": {0: 0.0, 1: 0.0, 2: 59.58605365589474},
+            "Amount_invested_monthly": {
+                0: 311.2433285195494,
+                1: 201.1301814828621,
+                2: 626.101931891704,
+            },
+            "Monthly_Balance": {
+                0: 273.5578381471173,
+                1: 1043.495568517138,
+                2: 581.6510144524012,
+            },
+            "Credit_Score": {0: "Good", 1: "Poor", 2: "Standard"},
+            "type_of_loan_Home Loan": {0: 1, 1: 0, 2: 0},
+            "type_of_loan_Car Loan": {0: 0, 1: 1, 2: 0},
+            "type_of_loan_Personal Loan": {0: 0, 1: 0, 2: 1},
+            "Spent_Habit": {
+                0: "Low_spent",
+                1: "High_spent",
+                2: "Low_spent",
+            },
+            "Payment_Habit": {
+                0: "Small_value_payments",
+                1: "Large_value_payments",
+                2: "Medium_value_payments",
+            },
+        }
+    )
+
+    pd.testing.assert_frame_equal(
+        result.sort_index(axis=1), expected_result.sort_index(axis=1)
+    )
+
 
 def test_process_age():
     mock_df = pd.DataFrame(
