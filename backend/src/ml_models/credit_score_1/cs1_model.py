0300b6611bab6e51d26db225369e3315065ac8ed
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-07-17 00:40:21 -0300

    refact: improve readability and adapt for the model changes

--- backend/src/ml_models/credit_score_1/cs1_model.py
+++ backend/src/ml_models/credit_score_1/cs1_model.py
@@ -13,86 +13,90 @@ log: Logger = getLogger(__name__)
 
 
 class Cs1Model:
+    """Class representing the CS1 Model for making predictions."""
+
+    MODEL_FILE_NAME = "cs1_model.z"
 
     def __init__(self) -> None:
-        MODEL_FILE_NAME = "cs1_model.z"
-        self.model_path: str = normpath(
-            join(dirname(__file__), MODEL_FILE_NAME)  # noqa: F821
-        )
-        self.accuracy: float = 0.0
-        self.__raw_model: Dict = None
-        self.estimator: Pipeline | BaseEstimator = None
-        self.coefficients: np.ndarray = None
-        self.features_names: np.ndarray = None
+        """Initialize the Cs1Model with default values and paths."""
+        self.model_path: str = normpath(join(dirname(__file__), self.MODEL_FILE_NAME))
 
-    def load(self) -> None:
-        """Loads the ML trained model (plus ancillary files) from file."""
+    def load(self) -> "Cs1Model":
+        """
+        Load the ML trained model from file.
+
+        Returns:
+            Cs1Model: The instance of the loaded Cs1Model.
+
+        Raises:
+            ModelNotLoaded: If the model fails to load.
+        """
         log.debug("Initializing the model from file %s", self.model_path)
-        self.__raw_model = joblib.load(self.model_path)
-        self.accuracy = self.__raw_model["test_score"][0]
-        self.estimator = self.__raw_model["estimator"][0].best_estimator_
-        self.classes = self.estimator.classes_
-        self.coefficients = self.estimator.named_steps.logreg.coef_
-        self.features_names = self.get_features_names()
-
-        log.debug("cs1_model loaded.")
+        try:
+            self.__raw_model: Dict = joblib.load(self.model_path)
+            self.accuracy: float = self.__raw_model["test_score"][0]
+            self.estimator: Pipeline | BaseEstimator = self.__raw_model["estimator"][
+                0
+            ].best_estimator_
+            self.classes: np.ndarray = self.estimator.classes_
+            self.coefficients: np.ndarray = self.estimator.named_steps["logreg"].coef_
+            self.features_names: np.ndarray = self.estimator.feature_names_in_
+            log.debug("cs1_model loaded successfully.")
+        except Exception as e:
+            log.error("Failed to load cs1_model: %s", e)
+            raise ModelNotLoaded(e)
 
         return self
 
-    def run(self, dto: PredictRequestDTO) -> Dict:
-        """Makes a prediction using the trained ML model."""
-        if self.estimator is None:
-            raise ModelNotLoaded(
-                "the cs1 model is not loaded, use the load() method first."
-            )
-
-        data = self.__dto_to_feature_df(dto)
-        probs = self.estimator.predict_proba(data).round(3)
-        return dict(zip(self.classes, probs[0]))
-
-    def get_features_names(self):
+    def run(self, dto: PredictRequestDTO) -> Dict[str, float]:
         """
-        Get the feature names from the loaded cs1 model.
-
-        Returns:
-            The feature names extracted from the model.
-        """
-        if self.estimator is None:
-            raise ModelNotLoaded(
-                "the cs1 model is not loaded, use the load() method first."
-            )
-
-        mask_features = self.estimator.named_steps["rfe"].get_support()
-        cols = self.estimator.named_steps["cols_trans"].get_feature_names_out()
-
-        return cols[mask_features]
-
-    @classmethod
-    def __dto_to_feature_df(
-        cls,
-        predict_request_dto: PredictRequestDTO,
-    ) -> pd.DataFrame:
-        """
-        Converts a PredictRequestDTO to a Pandas DataFrame. The additional columns are due to the model building.
-        These columns are not used in the prediction but are necessary for the model to work, thus the placeholder values.
+        Make a prediction using the trained ML model.
 
         Args:
-            predict_request_dto (PredictRequestDTO): The PredictRequestDTO to convert.
+            dto (PredictRequestDTO): The data transfer object containing input features.
+
+        Returns:
+            Dict[str, float]: A dictionary mapping class labels to their predicted probabilities.
+
+        Raises:
+            ModelNotLoaded: If the model is not loaded.
+        """
+        if self.estimator is None:
+            raise ModelNotLoaded(
+                "The cs1 model is not loaded, use the load() method first."
+            )
+
+        data = self._dto_to_feature_df(dto)
+        probabilities = self.estimator.predict_proba(data).round(3)
+        return dict(zip(self.classes, probabilities[0]))
+
+    @classmethod
+    def _dto_to_feature_df(cls, dto: PredictRequestDTO) -> pd.DataFrame:
+        """
+        Convert a PredictRequestDTO to a Pandas DataFrame.
+
+        Args:
+            dto (PredictRequestDTO): The data transfer object to convert.
 
         Returns:
             pd.DataFrame: The converted DataFrame.
         """
-        features_data = [predict_request_dto.features.model_dump()]
+        features_data = [dto.features.model_dump()]
         features_df = pd.DataFrame(features_data)
         features_df["income"] = features_df["income"].apply(
-            cls.__convert_monthly_income_to_annual_income
+            cls._convert_monthly_to_annual_income
         )
-        features_df["home_ownership"] = ["owned"]
-        features_df["number_of_children"] = [0]
-        features_df["marital_status"] = ["single"]
-
         return features_df
 
     @staticmethod
-    def __convert_monthly_income_to_annual_income(monthly_income):
+    def _convert_monthly_to_annual_income(monthly_income: float) -> float:
+        """
+        Convert monthly income to annual income.
+
+        Args:
+            monthly_income (float): The monthly income.
+
+        Returns:
+            float: The annual income.
+        """
         return monthly_income * 12
