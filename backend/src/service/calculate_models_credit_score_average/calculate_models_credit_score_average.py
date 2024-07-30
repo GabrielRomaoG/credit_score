9a37a542fce7946c82b5b6a9678922368ecd7333
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-07-29 23:48:54 -0300

    refact: wrap calculate method in a try block to implement logging for excepctions

--- backend/src/service/calculate_models_credit_score_average/calculate_models_credit_score_average.py
+++ backend/src/service/calculate_models_credit_score_average/calculate_models_credit_score_average.py
@@ -1,3 +1,8 @@
+from logging import Logger, getLogger
+
+log: Logger = getLogger(__name__)
+
+
 class ModelsCreditScoreAverageCalculator:
 
     @staticmethod
@@ -17,12 +22,18 @@ class ModelsCreditScoreAverageCalculator:
         Returns:
             float: The average credit score of the models.
         """
-        weighted_cs1_score = cs1_credit_score * cs1_model_accuracy
-        weighted_cs2_score = cs2_credit_score * cs2_model_accuracy
-        total_accuracy = cs1_model_accuracy + cs2_model_accuracy
 
-        weighted_average_score = (
-            weighted_cs1_score + weighted_cs2_score
-        ) / total_accuracy
+        try:
+            weighted_cs1_score = cs1_credit_score * cs1_model_accuracy
+            weighted_cs2_score = cs2_credit_score * cs2_model_accuracy
+            total_accuracy = cs1_model_accuracy + cs2_model_accuracy
 
-        return weighted_average_score
+            weighted_average_score = (
+                weighted_cs1_score + weighted_cs2_score
+            ) / total_accuracy
+
+            return weighted_average_score
+
+        except Exception as e:
+            log.error("Failed to calculate average credit score: %s", e)
+            raise e
