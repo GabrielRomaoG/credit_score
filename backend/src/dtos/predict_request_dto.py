0f21b09d0aa53dd7016b7758e1e4841d7256707c
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-08-09 18:48:06 -0300

    docs: add examples to PredictRequestDTO

--- backend/src/dtos/predict_request_dto.py
+++ backend/src/dtos/predict_request_dto.py
@@ -21,19 +21,19 @@ class Education(str, Enum):
 
 
 class Features(BaseModel):
-    age: int = Field(..., gt=0)
-    income: int = Field(..., gt=0)
-    gender: Gender
-    education: Education
-    num_bank_accounts: int = Field(..., gt=0)
-    num_credit_card: int = Field(..., gt=0)
-    num_of_loan: int = Field(..., gt=0)
-    num_of_delayed_payment: int = Field(..., gt=0)
-    outstanding_debt: int = Field(..., gt=0)
-    credit_history_age: int = Field(..., gt=0)
-    total_emi_per_month: int = Field(..., gt=0)
+    age: int = Field(..., gt=0, examples=[25, 30, 35, 40, 45])
+    income: int = Field(..., gt=0, examples=[6000, 7000, 8000, 9000, 10000])
+    gender: Gender = Field(..., examples=["male", "female"])
+    education: Education = Field(..., examples=["bachelors_degree"])
+    num_bank_accounts: int = Field(..., gt=0, examples=[1, 2, 3, 4, 5])
+    num_credit_card: int = Field(..., gt=0, examples=[1, 2, 3, 4, 5])
+    num_of_loan: int = Field(..., gt=0, examples=[1, 2, 3, 4, 5])
+    num_of_delayed_payment: int = Field(..., gt=0, examples=[1, 2, 3, 4, 5])
+    outstanding_debt: int = Field(..., gt=0, examples=[1000, 2000, 3000, 4000, 5000])
+    credit_history_age: int = Field(..., gt=0, examples=[1, 2, 3, 4, 5])
+    total_emi_per_month: int = Field(..., gt=0, examples=[1000, 2000, 3000, 4000, 5000])
 
 
 class PredictRequestDTO(BaseModel):
-    locale: Locale
-    features: Features
+    locale: Locale = Field(..., examples=["en-US", "pt-BR"])
+    features: Features = Field(...)
