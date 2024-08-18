75b24e7f3066499a1de94c735156a47ceb718fea
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-08-18 00:21:24 -0300

    fix: change some attributes of the PredictRequestDTO to be greater or equal to 0

--- backend/src/dtos/predict_request_dto.py
+++ backend/src/dtos/predict_request_dto.py
@@ -22,16 +22,16 @@ class Education(str, Enum):
 
 class Features(BaseModel):
     age: int = Field(..., gt=0, examples=[25])
-    income: int = Field(..., gt=0, examples=[6000], alias="monthly_income")
+    income: int = Field(..., ge=0, examples=[6000], alias="monthly_income")
     gender: Gender = Field(..., examples=["male"], alias="sex")
     education: Education = Field(..., examples=["bachelors_degree"])
-    num_bank_accounts: int = Field(..., gt=0, examples=[1])
-    num_credit_card: int = Field(..., gt=0, examples=[2])
-    num_of_loan: int = Field(..., gt=0, examples=[3])
-    num_of_delayed_payment: int = Field(..., gt=0, examples=[2])
-    outstanding_debt: int = Field(..., gt=0, examples=[2000])
-    credit_history_age: int = Field(..., gt=0, examples=[3])
-    total_emi_per_month: int = Field(..., gt=0, examples=[2000])
+    num_bank_accounts: int = Field(..., ge=0, examples=[1])
+    num_credit_card: int = Field(..., ge=0, examples=[2])
+    num_of_loan: int = Field(..., ge=0, examples=[3])
+    num_of_delayed_payment: int = Field(..., ge=0, examples=[2])
+    outstanding_debt: int = Field(..., ge=0, examples=[2000])
+    credit_history_age: int = Field(..., ge=0, examples=[3])
+    total_emi_per_month: int = Field(..., ge=0, examples=[2000])
 
 
 class PredictRequestDTO(BaseModel):
