1a1b87f457c1660a98264372298d9556de866eab
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-08-26 00:23:14 -0300

    feat: add responsivity to DefaultProfiles and Profile

--- frontend/src/lib/DefaultProfiles/DefaultProfiles.svelte
+++ frontend/src/lib/DefaultProfiles/DefaultProfiles.svelte
@@ -1,34 +1,29 @@
 <script>
-	// Import the Profile component
 	import Profile from './Profile.svelte';
 
-	// Expect an array of profiles to be passed in as a prop
 	export let profiles = [
 		{
 			imageUrl: 'https://via.placeholder.com/64',
 			altText: 'Profile 1',
-			labelText: 'User 1'
+			labelText: 'Jovem Estudante'
 		},
 		{
 			imageUrl: 'https://via.placeholder.com/64',
 			altText: 'Profile 2',
-			labelText: 'User 2'
+			labelText: 'Profissional Experiente'
 		},
 		{
 			imageUrl: 'https://via.placeholder.com/64',
 			altText: 'Profile 3',
-			labelText: 'User 3'
+			labelText: 'Profissional de meia-idade'
 		}
 	];
 </script>
 
-<div class="rounded-lg border bg-white p-4 shadow-lg">
-	<!-- Static Title -->
+<div class="max-w-[600px] grow self-center rounded-lg border bg-white p-4 shadow-lg">
 	<h2 class="mb-4 text-center text-lg font-semibold">Check Default Profiles</h2>
 
-	<!-- Profiles Container -->
-	<div class="flex justify-between space-x-2">
-		<!-- Render each profile dynamically -->
+	<div class="flex justify-between">
 		{#each profiles as profile (profile.labelText)}
 			<Profile
 				imageUrl={profile.imageUrl}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,8 +3,10 @@
 	import LL from '$i18n/i18n-svelte';
 </script>
 
-<main class="top-[100vh] flex w-full items-center justify-between px-44 py-8">
-	<div class="flex w-full justify-between">
+<main
+	class="mx-auto flex w-11/12 max-w-6xl items-center justify-center py-8 md:w-4/5 xl:justify-between"
+>
+	<div class="flex w-full flex-col gap-8 min-[1005px]:flex-row xl:gap-32">
 		<div>
 			<h1 class="text-6.5xl font-bold">{$LL.credit_score_ai()}</h1>
 			<h2 class="text-2xl">
