8dfbb92ee37af899d2a3149b0025147303f7d2ff
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-08-30 02:10:12 -0300

    feat: Implement Form/Button component

---
+++ frontend/src/lib/Form/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let backgroundColor: string = 'bg-blue-500';
+	export let textColor: string = 'text-white';
+	export let borderColor: string = 'border-transparent';
+	export let basis: string = 'basis-full'; // Default to full width
+	export let label: string = 'Button';
+	export let onClick: (event: Event) => void = () => {};
+</script>
+
+<button
+	on:click={onClick}
+	class={`rounded-full px-6 py-2 text-base ${backgroundColor} ${textColor} ${borderColor} ${basis} grow border-2`}
+>
+	{label}
+</button>
