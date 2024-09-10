6fe7c4e466ec4e2e229d77a46dd1e828e4c29f2b
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-09-09 22:30:26 -0300

    feat: add isSubmit prop in the Button component

--- frontend/src/lib/Form/Button.svelte
+++ frontend/src/lib/Form/Button.svelte
@@ -5,9 +5,11 @@
 	export let basis: string = 'basis-full'; // Default to full width
 	export let label: string = 'Button';
 	export let onClick: (event: Event) => void = () => {};
+	export let isSubmit: boolean = false;
 </script>
 
 <button
+	type={isSubmit ? 'submit' : 'button'}
 	on:click={onClick}
 	class={`rounded-full px-6 py-2 text-base ${backgroundColor} ${textColor} ${borderColor} ${basis} grow border-2`}
 >
