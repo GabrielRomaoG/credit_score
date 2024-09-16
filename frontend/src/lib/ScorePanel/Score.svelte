17f76cc2158b6295f886324fc11bf5741ca0ea19
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-09-16 03:55:55 -0300

    fix: adapt Score component for progressive enhancement

--- frontend/src/lib/ScorePanel/Score.svelte
+++ frontend/src/lib/ScorePanel/Score.svelte
@@ -2,78 +2,52 @@
 	import Speedometer from 'svelte-speedometer';
 	export let isActive: boolean = false;
 	export let score: number = 450;
-
-	// Determine the Speedometer settings based on the score
-	let segmentColors: Array<string> = [];
-	let customSegmentLabels: Array<{ text: string; position: string; color: string }> = [];
-	let minValue = 250;
-	let maxValue = 900;
-
-	if (isActive) {
-		// Define the ranges for each segment
-		const lowRange = [minValue, (maxValue - minValue) / 3 + minValue];
-		const averageRange = [
-			(maxValue - minValue) / 3 + minValue,
-			((maxValue - minValue) * 2) / 3 + minValue
-		];
-		const highRange = [((maxValue - minValue) * 2) / 3 + minValue, maxValue];
-
-		// Conditional colors and labels based on score
-		segmentColors = [
-			score >= lowRange[0] && score < lowRange[1] ? '#EE6B6B' : '#D3D3D3', // Low segment
-			score >= averageRange[0] && score < averageRange[1] ? '#F4E347' : '#A9A9A9', // Average segment
-			score >= highRange[0] && score <= highRange[1] ? '#8ED731' : '#808080' // High segment
-		];
-
-		customSegmentLabels = [
-			{
-				text: score >= lowRange[0] && score < lowRange[1] ? 'Low' : '',
-				position: 'OUTSIDE',
-				color: '#EE6B6B'
-			},
-			{
-				text: score >= averageRange[0] && score < averageRange[1] ? 'Average' : '',
-				position: 'OUTSIDE',
-				color: '#DEA740'
-			},
-			{
-				text: score >= highRange[0] && score <= highRange[1] ? 'High' : '',
-				position: 'OUTSIDE',
-				color: '#75B028'
-			}
-		];
-	} else if (!isActive) {
-		// If inactive, all segments are gray
-		segmentColors = ['#D3D3D3', '#A9A9A9', '#808080'];
-		customSegmentLabels = [
-			{ text: '', position: 'OUTSIDE', color: '#808080' },
-			{ text: '', position: 'OUTSIDE', color: '#A9A9A9' },
-			{ text: '', position: 'OUTSIDE', color: '#D3D3D3' }
-		];
-		minValue = 0;
-		maxValue = 1;
-	}
 </script>
 
 <div class="flex flex-col items-center">
 	<Speedometer
 		height={160}
 		width={260}
-		{minValue}
-		{maxValue}
-		value={!isActive ? 0 : score}
-		currentValueText=""
+		minValue={isActive ? 250 : 0}
+		maxValue={isActive ? 900 : 1}
+		value={isActive ? score : 0}
+		currentValueText=" "
 		customSegmentStops={[
-			minValue,
-			(maxValue - minValue) / 3 + minValue,
-			((maxValue - minValue) * 2) / 3 + minValue,
-			maxValue
+			isActive ? 250 : 0,
+			isActive ? (900 - 250) / 3 + 250 : 0.33,
+			isActive ? ((900 - 250) * 2) / 3 + 250 : 0.66,
+			isActive ? 900 : 1
 		]}
-		{segmentColors}
-		{customSegmentLabels}
+		segmentColors={isActive
+			? [
+					score >= 250 && score < 466 ? '#EE6B6B' : '#D3D3D3',
+					score >= 466 && score < 684 ? '#F4E347' : '#A9A9A9',
+					score >= 684 && score <= 900 ? '#8ED731' : '#808080'
+				]
+			: ['#D3D3D3', '#A9A9A9', '#808080']}
+		customSegmentLabels={isActive
+			? [
+					{ text: score >= 250 && score < 466 ? 'Low' : '', position: 'OUTSIDE', color: '#EE6B6B' },
+					{
+						text: score >= 466 && score < 684 ? 'Average' : '',
+						position: 'OUTSIDE',
+						color: '#DEA740'
+					},
+					{
+						text: score >= 684 && score <= 900 ? 'High' : '',
+						position: 'OUTSIDE',
+						color: '#75B028'
+					}
+				]
+			: [
+					{ text: '', position: 'OUTSIDE', color: '#808080' },
+					{ text: '', position: 'OUTSIDE', color: '#A9A9A9' },
+					{ text: '', position: 'OUTSIDE', color: '#D3D3D3' }
+				]}
 		ringWidth={15}
 		needleColor="black"
 		needleHeightRatio={0.7}
+		forceRender={true}
 	/>
 
 	{#if isActive}
