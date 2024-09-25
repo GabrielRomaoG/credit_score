ce35b7512159f5b717e6eca98e72c31e5e366956
Gabriel Romao <gabriel-rgomes@hotmail.com>
2024-09-24 23:33:11 -0300

    fix: change the default values for the numeric inputs to 0

--- frontend/src/lib/schemas.ts
+++ frontend/src/lib/schemas.ts
@@ -10,35 +10,29 @@ const numericFieldSchema = (fieldName: string, LL: TranslationFunctions) =>
 
 export const featuresSchema = (LL: TranslationFunctions, defaultValues?: Features) =>
 	z.object({
-		age: numericFieldSchema('Age', LL)
-			.gt(0)
-			.default(defaultValues?.age ?? ('' as unknown as number)),
+		age: numericFieldSchema('Age', LL).default(defaultValues?.age ?? 0),
 		monthly_income: numericFieldSchema('Monthly income', LL).default(
-			defaultValues?.monthly_income ?? ('' as unknown as number)
+			defaultValues?.monthly_income ?? 0
 		),
-		sex: z.nativeEnum(Sex).default(defaultValues?.sex ?? Sex.Male),
-		education: z
-			.nativeEnum(Education)
-			.default(defaultValues?.education ?? Education.BachelorsDegree),
+		sex: z.nativeEnum(Sex).default(defaultValues?.sex ?? ('' as Sex)),
+		education: z.nativeEnum(Education).default(defaultValues?.education ?? ('' as Education)),
 		num_bank_accounts: numericFieldSchema('Number of bank accounts', LL).default(
-			defaultValues?.num_bank_accounts ?? ('' as unknown as number)
+			defaultValues?.num_bank_accounts ?? 0
 		),
 		num_credit_card: numericFieldSchema('Number of credit cards', LL).default(
-			defaultValues?.num_credit_card ?? ('' as unknown as number)
+			defaultValues?.num_credit_card ?? 0
 		),
-		num_of_loan: numericFieldSchema('Number of loan', LL).default(
-			defaultValues?.num_of_loan ?? ('' as unknown as number)
-		),
+		num_of_loan: numericFieldSchema('Number of loan', LL).default(defaultValues?.num_of_loan ?? 0),
 		num_of_delayed_payment: numericFieldSchema('Number of delayed payment', LL).default(
-			defaultValues?.num_of_delayed_payment ?? ('' as unknown as number)
+			defaultValues?.num_of_delayed_payment ?? 0
 		),
 		outstanding_debt: numericFieldSchema('Outstanding debt', LL).default(
-			defaultValues?.outstanding_debt ?? ('' as unknown as number)
+			defaultValues?.outstanding_debt ?? 0
 		),
 		credit_history_age: numericFieldSchema('Credit history age', LL).default(
-			defaultValues?.credit_history_age ?? ('' as unknown as number)
+			defaultValues?.credit_history_age ?? 0
 		),
 		total_emi_per_month: numericFieldSchema('Total EMI per month', LL).default(
-			defaultValues?.total_emi_per_month ?? ('' as unknown as number)
+			defaultValues?.total_emi_per_month ?? 0
 		)
 	});
